d8f1096b7675167d1820085092dacad66fbefbd6
JoseManuelValderrama73 <jmvaldesan2@gmail.com>
2025-06-25 20:47:24 +0200

    3.1.1 UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from emisor import Emisor
 from receptor import Receptor
 import asyncio
+from kernel import printerr
 
 async def servidor(puerto: int):
     servidor = Receptor(puerto)
@@ -12,13 +13,29 @@ async def cliente(msg: str, N: int, host: str, port: int):
 
 opcion = int(input("Escuchar mensajes (1)\nEnviar mensaje (2)\n-> "))
 if opcion == 1:
-    puerto=int(input("Puerto a escuchar: "))
-    asyncio.run(servidor(puerto))
+    asyncio.run(servidor(
+        int(input("Puerto a escuchar: "))
+    ))
 elif opcion == 2:
-    msg=input("Mensaje a enviar: "),
-    n=int(input("Número de bits de seguridad: ")),
-    host=input("Host del receptor: "),
-    port=int(input("Puerto del receptor: "))
+    host = input("Host del receptor: ")
+    port = int(input("Puerto del receptor: "))
+
+    msg = input("Mensaje a enviar: ")
+    while msg == "":
+        printerr('El mensaje no puede estar vacío.')
+        msg = input("Mensaje a enviar: ")
+
+    n = int(input("Número de bits de seguridad: "))
+    while n < 1:
+        printerr('El número de bits de seguridad debe ser mayor que 0.')
+        n = int(input("Número de bits de seguridad: "))
+    if n > len(msg):
+        print('El numero de bits de seguridad no debe ser demasiado grande.\nArriesga un fallo en el envío del mensaje así como menos seguridad.')
+        n = int(input("Número de bits de seguridad: "))
+        while n < 1:
+            printerr('El número de bits de seguridad debe ser mayor que 0.')
+            n = int(input("Número de bits de seguridad: "))
+
     asyncio.run(cliente(msg, n, host, port))
 else:
-    print("Opción no válida")
+    printerr("Opción no válida")
